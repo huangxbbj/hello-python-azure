d0f7f70d16ab36240b93c843935c5b984f798b2e
huangxbbj <huangxb.bj@gmail.com>
2026-04-21 21:26:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello Azure CI/CD 🚀 Version 8"
+def hello():
+    return "Hello Azure! CI/CD 🚀 Version 1"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
